1fa8ad5d7683a1a919c193d355a4332fd229a3d5
TristanStreich <tristanstreich@gmail.com>
2026-05-10 17:14:59 -0700

    basic textbox

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -11,6 +11,10 @@ cargo build --target wasm32-unknown-unknown --bin $GAME
 
 wasm-bindgen --out-dir ./wasm --target web target/wasm32-unknown-unknown/debug/${GAME}.wasm --out-name game
 
+if [ -d "assets" ]; then
+    cp -r assets wasm/
+fi
+
 if [[ "$*" == *"--run"* ]]; then
     python3 -m http.server 2424 --directory wasm
 fi
